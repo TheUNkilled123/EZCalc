e4c7cdf8f13ee10aa3507f4d00315e19cc0c2bc8
Toni <toni.jelasac@gmail.com>
2021-03-21 15:55:18 +0100

    Added delayed "splash" activity on launch.

--- app/src/main/java/com/example/ezcalc/MainActivity.java
+++ app/src/main/java/com/example/ezcalc/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.ezcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import com.fathzer.soft.javaluator.DoubleEvaluator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DoubleEvaluator evaluator = new DoubleEvaluator();
+
+
+
+
+
+        //All code below is only for testing purposes and not final -- Sav kod ispod je samo za test i nije finalan.
+
+        TextView view = findViewById(R.id.MainView);
+        Button calc = findViewById(R.id.solve);
+        TextInputEditText text = findViewById(R.id.toSolve);
+        Button addP = findViewById(R.id.zagradaTest);
+
+        addP.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                int stringlen = text.length();
+                if ((text.getText().charAt(stringlen - 1) != '*') || (text.getText().charAt(stringlen - 1) != '/')){
+                    //int temp = text.getText();
+                    text.getText().append("*(");
+                }
+            }
+        });
+
+        calc.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                view.setText(String.valueOf(evaluator.evaluate(String.valueOf(text.getText()))));
+            }
+        });
+
     }
 }
